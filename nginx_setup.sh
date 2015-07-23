43fe048e451b8329b830918d9be32acc82b22e8d
Georgy Ivanov <ivanov.georg@gmail.com>
2015-07-23 04:00:45 +0300

    nginx_setup

---
+++ nginx_setup.sh
@@ -0,0 +1,30 @@
+cd /usr/share/nginx/html
+mkdir scores
+cd scores
+
+for i in {1..15}
+do
+  cd /usr/share/nginx/html/scores
+  n=${i}
+  if [[ "$n" -le "9" ]] 
+  then
+     n="0${i}"
+  fi
+  echo $n;
+  pwd
+  sudo ln -s "/home/user/coding/ale_team_runner/teams/team_170${n}" "team${i}"
+#  ls -al ./
+  pwd
+  cd "team${i}"
+  sudo htpasswd -c .htpasswd team${i}
+
+  cd /usr/share/nginx/html/scores
+
+  echo "location /scores/team${i} {
+    autoindex on;
+    auth_basic \"Restricted\";
+    auth_basic_user_file /usr/share/nginx/html/scores/team${i}/.htpasswd;
+  }
+  " >> ~/nginx_conf_str
+
+done
